74ce913eb4e5d4295d281420ca4d67e525d6d79a
eidng8 <cheung.jackey@gmail.com>
2021-01-06 11:44:04 +0800

    init commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class AuthController extends Controller
+{
+    /**
+     * Store a new user.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     * @throws ValidationException
+     */
+    public function register(Request $request): Response
+    {
+        //validate incoming request
+        $this->validate(
+            $request,
+            [
+                'name' => 'required|string',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|confirmed',
+            ]
+        );
+
+        try {
+            $user = new User();
+            $user->name = $request->input('name');
+            $user->email = $request->input('email');
+            $plainPassword = $request->input('password');
+            $user->password = app('hash')->make($plainPassword);
+
+            $user->save();
+
+            //return successful response
+            return response()->json(
+                ['user' => $user, 'message' => 'CREATED'],
+                201
+            );
+        } catch (Exception $e) {
+            //return error message
+            return response()->json(
+                ['message' => 'User Registration Failed!'],
+                409
+            );
+        }
+    }
+}
---
+++ tests/Units/Http/Controllers/AuthControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Units\Http\Controllers;
+
+use Tests\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+
+    public function testRegister()
+    {
+    }
+}

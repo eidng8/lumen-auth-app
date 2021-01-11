64d910dd338b546c304276c7bfe9cf23b4f29332
eidng8 <cheung.jackey@gmail.com>
2021-01-11 14:16:10 +0800

    add WSL start script

---
+++ start.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#
+# GPLv3  https://www.gnu.org/licenses/gpl-3.0-standalone.html
+#
+# author eidng8
+#
+#
+
+if [ "-h" == "$1" ] || [ "--help" == "$1" ]; then
+    echo "start.sh [swoole]"
+    echo "Starts a swoole server, or default PHP dev server if omitted."
+    exit
+fi
+
+echo Box IP address:
+ip address | grep -F --color=never 'scope global'
+
+# shellcheck disable=SC2164
+WD="$(
+    cd "$(dirname "$0")" >/dev/null 2>&1
+    pwd -P
+)"
+
+USE_IP=0.0.0.0
+USE_PORT=8000
+
+DB_DATABASE="$WD"/storage/app/database.sqlite
+
+if [ "swoole" == "$1" ]; then
+    DB_DATABASE="$DB_DATABASE" SWOOLE_HTTP_HOST="$USE_IP" php "$WD/artisan" swoole:http start
+    exit
+fi
+
+DB_DATABASE="$DB_DATABASE" php -S "$USE_IP:$USE_PORT" -t "$WD/public"
